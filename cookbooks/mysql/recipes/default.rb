7c3d616b8766dc06670a09e6444c7cacd96179c4
Danilo Braband <d.braband@k-newmedia.de>
2013-07-09 08:50:33 +0200

    Fixed an error with the mysql recipe.

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -12,27 +12,21 @@ service "mysql" do
 end
 
 # set the root password
-execute "assign-root-password" do
-  command "/usr/bin/mysqladmin -uroot password #{mysql_root_pwd}"
+execute "/usr/bin/mysqladmin -uroot password #{mysql_root_pwd}" do
   not_if "/usr/bin/mysqladmin -uroot -p#{mysql_root_pwd} status"
 end
 
 # create the default database
-execute "create-default-database" do
-  command "/usr/bin/mysql -uroot -p#{mysql_root_pwd} -e 'CREATE DATABASE IF NOT EXISTS #{mysql_default_db}'"
-end
+execute "/usr/bin/mysql -uroot -p#{mysql_root_pwd} -e 'CREATE DATABASE IF NOT EXISTS #{mysql_default_db}'"
 
 # grant privileges to the user so that he can get access from the host machine
-execute "grant-access-to-root" do
-  command "/usr/bin/mysql -uroot -p#{mysql_root_pwd} -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '#{mysql_root_pwd}' WITH GRANT OPTION;\""
-end
+execute "/usr/bin/mysql -uroot -p#{mysql_root_pwd} -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '#{mysql_root_pwd}' WITH GRANT OPTION;\""
 
-execute "bind-mysql-address" do
-  not_if "cat #{mysql_config_file} | grep 127.0.0.1"
-  command "set -i 's/127.0.0.1/0.0.0.0/g' #{mysql_config_file}"
+execute "sed -i 's/127.0.0.1/0.0.0.0/g' #{mysql_config_file}" do
+  not_if "cat #{mysql_config_file} | grep 0.0.0.0"
 end
 
 service "mysql" do
-  action :start
+  action :restart
 end
 
